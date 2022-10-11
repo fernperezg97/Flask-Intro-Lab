2269cb44fda175eecb4c1bc043fb554d0875379a
fernperezg97 <fernandaperezgutierrez1@gmail.com>
2022-10-11 16:00:47 -0500

    finished compliments
    co-authored-by: fernperezg97 fernandaperezgutierrez1@gmail.com
    co-authored-by: maryamabdulle iammaryamabdulle@gmail.com

---
+++ server.py
@@ -0,0 +1,105 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+    <html>
+      <body>
+        <h1>Hi! This is the homepage.</h1>
+        <a href="/hello"> say hello</a>
+      </body>
+    </html>
+    """
+
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person"><br>
+    
+        <label for="compliments">Choose a compliment:</label>
+
+        <select name="compliments" id="compliments">
+          <option value="awesome">Awesome</option>
+          <option value="terrific">terrific</option>
+          <option value="fantastic">fantastic</option>
+          <option value="neato">neato</option>\
+          <option value="fantabulous">fantabulous</option>
+          <option value="wowza">wowza</option>
+          <option value="oh-so-not-meh">oh-so-not-meh</option>
+          <option value="ducky">ducky</option>
+          <option value="coolio">coolio</option>
+          <option value="incredible">incredible</option>
+
+
+          <option value="wonderful">wonderful</option>
+          <option value="smashing">smashing</option>
+          <option value="lovely">lovely</option>
+        </select>
+
+
+
+
+
+
+        <br>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
